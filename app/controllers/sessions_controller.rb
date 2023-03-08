f2e758b9317b5eaa34b35b3845760999ee343950
adenaschmidt <adena.schmidt@kellogg.northwestern.edu>
2023-03-08 20:55:24 +0000

    initialcomm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+      if params["password"] == @user["password"]
+      redirect_to "/posts"
+    else
+     redirect_to "/sessions/new"
+    end
+  else
+    redirect_to "/sessions/new"
+  end
   end
 
   def destroy
   end
+
 end
   
