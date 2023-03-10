4d33ec36f0476255d58ad3901417dcb24d5f6bd1
adenaschmidt <adena.schmidt@kellogg.northwestern.edu>
2023-03-10 03:55:33 +0000

    late

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,9 @@
 class PostsController < ApplicationController
 
-  
   def index
     @posts = Post.all
   end
 
-  def show
-    @posts = Post.find_by({"id" => params["id"]})
-    @place = Place.find_by({"id" => @post["place_id"]})
-  end
 
   def new
     @post = Post.new
@@ -16,18 +11,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
-    @post["image"] = params["post"]["image"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
     @post.save
-    else
-      flash["notice"] = "You must login first."
-    end
+    
     redirect_to "/places/#{@post["place_id"]}"
   end
 
